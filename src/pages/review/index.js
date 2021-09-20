e3c6859d85847e965528b43ca2a23965d8ef9de0
Allan dos Santos <allan.gama720@gmail.com>
2021-09-19 21:28:05 -0300

    adicionei a nóva página de login ao repositório

--- src/pages/review/index.js
+++ src/pages/review/index.js
@@ -1,11 +1,60 @@
 import React from 'react';
 import '../../styles/review.scss';
 
+import Header from "../../components/header";
+import SideBar from "../../components/sidebar";
+
 const review = () => {
     return(
-      <div>
-        <h1>Reveja!!!</h1>
-      </div>
+
+     <div id="global-container">
+      
+         <Header/>
+       
+
+       <div id="main-container">
+          
+
+          <SideBar/>
+
+          <div id="review-container">
+            <div id="review-title">
+              <h3>Avaliação de Reprografia</h3>
+            </div>
+
+            <div id="review-content">
+              <div id="feedback-checkbox">
+                
+                <div className="checkbox">
+                  <label for="nao-atendeu">Não Atendeu</label>
+                  <input type="checkbox" name="nao-atendeu" id="nao-atendeu" className="checkbox-avaliacao"/>
+                </div>
+
+                <div className="checkbox">
+                  <label for="atendeu">Atendeu</label>
+                  <input type="checkbox" name="atendeu" id="atendeu" className="checkbox-avaliacao"/>
+                </div>
+
+                <div className="checkbox">
+                  <label for="superou">Superou</label>
+                  <input type="checkbox" name="superou" id="superou" className="checkbox-avaliacao"/>
+                </div>
+
+              </div>
+
+              <div id="feedback-text">
+                <textarea placeholder=" digite seu feedback"></textarea>
+              </div>
+                
+            </div>
+            <div id="button-review">
+
+              <button id="review-button"> Enviar Avaliação</button>
+            </div>
+          </div>
+       </div>
+     </div>
+ 
     );
 }
 
