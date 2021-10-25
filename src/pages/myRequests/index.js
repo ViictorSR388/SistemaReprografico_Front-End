8107ec75691f20ff33f1636cfe54b5cbbf670632
Lucca <lucca@pop-os.localdomain>
2021-10-25 10:14:15 -0300

    Criando state de Loading

--- src/pages/myRequests/index.js
+++ src/pages/myRequests/index.js
@@ -16,8 +16,10 @@ const MyRequests = () => {
 
     var [nameUser, setNameUser] = useState();
     var [image, setImage] = useState();
+    var [loading, setLoading] = useState();
 
     useEffect(() => {
+        setLoading(true)
         axios.get("http://localhost:3002/meusPedidos", {
             headers: {
                 accessToken: localStorage.getItem("accessToken"),
@@ -70,47 +72,49 @@ const MyRequests = () => {
         }).then((result) => {
             setImage("http://localhost:3002/" + result.data.imagem)
             setNameUser(result.data.nome)
+            setLoading(false)
         })
     }, []);
 
     return (
         <>
-            <div className="content">
 
-                <ProfileContainer requestsNoInfo="true" source={image} name={nameUser} changePassword={() => {
+            <div className="content">
+                {loading ? <> Loading...</> : <><ProfileContainer requestsNoInfo="true" source={image} name={nameUser} changePassword={() => {
                     history.push("/userInfo")
                 }} />
-                <div className="container">
-                    {pedidos.status ?
-                        <>
-                            {pedidos.list.map((data) => (
-                                <React.Fragment key={data.id_pedido}> {/* é a mesma coisa que <> ... <> é a abreviação de <React.Fragment>
+                    <div className="container">
+                        {pedidos.status ?
+                            <>
+                                {pedidos.list.map((data) => (
+                                    <React.Fragment key={data.id_pedido}> {/* é a mesma coisa que <> ... <> é a abreviação de <React.Fragment>
                             A key é para referenciar uma key única para o map não se perder... e também parar de dar erro no console */}
 
-                                    <div /* style={data.criado} */ > {/* Setei a cor das palavras dentro da div lá em cima no UseEffect para estar passando um estilo diferente 
+                                        <div /* style={data.criado} */ > {/* Setei a cor das palavras dentro da div lá em cima no UseEffect para estar passando um estilo diferente 
                                     se um pedido já tiver sido atualizado (vocês podem alterar depois a forma que querem exibir essa diferença, pode ser um border, talvez...) */}
 
-                                        <h2>Pedido{/*<N.ºspan>{data.id_pedido}</span>*/}:<span>{data.titulo_pedido}</span></h2>
-                                        {/* <h2>Realizado pelo usuário com Nif: <span>{data.nif}</span> </h2> */}
-                                        <h2>Centro de custos: <span>{data.centro_custos}</span></h2>
+                                            <h2>Pedido{/*<N.ºspan>{data.id_pedido}</span>*/}:<span>{data.titulo_pedido}</span></h2>
+                                            {/* <h2>Realizado pelo usuário com Nif: <span>{data.nif}</span> </h2> */}
+                                            <h2>Centro de custos: <span>{data.centro_custos}</span></h2>
 
-                                        <h2>status: <span>{data.avaliacao_pedido}</span></h2>
-                                    </div>
-                                    <button onClick={() => { history.push("/detPedido/" + data.id_pedido) }}>detalhes</button>
-                                    {data.avaliado ? <></> : <button onClick={() => { history.push("/review/" + data.id_pedido) }}>Avaliar</button>}
-                                    <h3>----------------------------</h3>  {/* Só coloquei para separar, para nao
+                                            <h2>status: <span>{data.avaliacao_pedido}</span></h2>
+                                        </div>
+                                        <button onClick={() => { history.push("/detPedido/" + data.id_pedido) }}>detalhes</button>
+                                        {data.avaliado ? <></> : <button onClick={() => { history.push("/review/" + data.id_pedido) }}>Avaliar</button>}
+                                        <h3>----------------------------</h3>  {/* Só coloquei para separar, para nao
                 ter que mexer no css de voces e bagunçar algo lá... */}
 
-                                </React.Fragment>
-                            ))}
+                                    </React.Fragment>
+                                ))}
 
-                        </> :
-                        <>
-                            <h1>{pedidos.message}</h1>
-                        </>
-                    }
-                    <button onClick={() => { history.push("/userInfo") }}> Voltar </button>
-                </div>
+                            </> :
+                            <>
+                                <h1>{pedidos.message}</h1>
+                            </>
+                        }
+                        <button onClick={() => { history.push("/userInfo") }}> Voltar </button>
+                    </div> </>}
+
             </div>
         </>
     )
