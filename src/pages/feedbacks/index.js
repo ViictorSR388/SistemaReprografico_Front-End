3ff16a45074ed2eada01aac6291712c940e561a7
Lucca <mario-lucca@hotmail.com>
2021-12-08 17:57:58 -0300

    Arrumando link das requisições da página feedbacks

--- src/pages/feedbacks/index.js
+++ src/pages/feedbacks/index.js
@@ -13,132 +13,132 @@ import Loading from "../../components/loading";
 
 function RequestList(props) {
 
-  const { id } = useParams();
+    const { id } = useParams();
 
-  var [feedbacks, setFeedBacks] = useState({
-    status: false,
-    list: [],
-    message: "",
-  });
+    var [feedbacks, setFeedBacks] = useState({
+        status: false,
+        list: [],
+        message: "",
+    });
 
-  const port = process.env.REACT_APP_PORT || 3002;
-  
-  const reprografia_url = `${process.env.REACT_APP_REPROGRAFIA_URL}:${port}`;
+    const port = process.env.REACT_APP_PORT || 3002;
 
-  const Details = (feedbackId) => {
-    axios
-      .get(`${reprografia_url}/${feedbackId}`, {
-        headers: {
-          accessToken: localStorage.getItem("accessToken"),
-        },
-      })
-      .then((result) => {
-        console.log(result)
-        if (result.data.status === "error" || result.status === 500 || result.status === 403) {
-          Swal.fire(
-            `ERROR!`,
-            result.data.message
-          )
-        }
-        else {
-          Swal.fire(
-            `Detalhes da avaliação Nº${feedbackId}`,
-            `Solicitada pelo usuário com nif <strong>${result.data.userId}</strong> no pedido <strong>Nº${result.data.pedidoId}</strong> <br></br> <h3><strong>Avaliação:</strong></h3> ${result.data.avaliacao_obs}`,
-            'warning'
-          )
-        }
-      });
-  }
-  var [loading, setLoading] = useState(Loading);
+    const reprografia_url = `${process.env.REACT_APP_REPROGRAFIA_URL}:${port}`;
 
-  useEffect(() => {
-      setLoading(true)
-    axios
-      .get(`http://localhost:3002/feedbacks/${id}`, {
-        headers: {
-          accessToken: localStorage.getItem("accessToken"),
-        },
-      })
-      .then((result) => {
-        if (result.data.status === "error") {
-          setFeedBacks({
-            message: result.data.message
-          });
-        } else {
-          setFeedBacks({
-            list: result.data,
-            status: true,
-          });
-        }
-        setLoading(false)
-      });
-  }, [id]);
+    const Details = (feedbackId) => {
+        axios
+            .get(`${reprografia_url}/feedback/id/${feedbackId}`, {
+                headers: {
+                    accessToken: localStorage.getItem("accessToken"),
+                },
+            })
+            .then((result) => {
+                console.log(result)
+                if (result.data.status === "error" || result.status === 500 || result.status === 403) {
+                    Swal.fire(
+                        `ERROR!`,
+                        result.data.message
+                    )
+                }
+                else {
+                    Swal.fire(
+                        `Detalhes da avaliação Nº${feedbackId}`,
+                        `Solicitada pelo usuário com nif <strong>${result.data.userId}</strong> no pedido <strong>Nº${result.data.pedidoId}</strong> <br></br> <h3><strong>Avaliação:</strong></h3> ${result.data.avaliacao_obs}`,
+                        'warning'
+                    )
+                }
+            });
+    }
+    var [loading, setLoading] = useState(Loading);
 
-  return (
-<>
-{loading ? <Loading /> : 
-    <>
-    <Menu />
-    <Header nif={props.nif} />
-    <SideBar image={props.image} name={props.name} admin={props.admin} />
+    useEffect(() => {
+        setLoading(true)
+        axios
+            .get(`${reprografia_url}/feedbacks/${id}`, {
+                headers: {
+                    accessToken: localStorage.getItem("accessToken"),
+                },
+            })
+            .then((result) => {
+                if (result.data.status === "error") {
+                    setFeedBacks({
+                        message: result.data.message
+                    });
+                } else {
+                    setFeedBacks({
+                        list: result.data,
+                        status: true,
+                    });
+                }
+                setLoading(false)
+            });
+    }, [id, reprografia_url]);
 
-    <div className="container-feedbacks">
-      <div className="titleFDB">
-        <h1>Avaliações do pedido Nº{id}</h1>
-      </div>
-    </div>
-
-    <div className="container-feedbacks">
-      <>
-        {feedbacks.status ? (
-          <>
-            <Table className="table-feedbacks" striped bordered hover size="sm">
-              <thead>
-                <th> Realizado por (nif): </th>
-                <th>
-                  Realizado em:
-                </th>
-                <th>Status</th>
-                <th>
-                  Detalhes
-                </th>
-              </thead>
-              {feedbacks.list.map((data) => (
+    return (
+        <>
+            {loading ? <Loading /> :
                 <>
-                  <tbody>
-                    <tr>
-                      <td>
-                        <h1>{data.userId}</h1>
-                      </td>
+                    <Menu />
+                    <Header nif={props.nif} />
+                    <SideBar image={props.image} name={props.name} admin={props.admin} />
 
-                      <td>
-                        <h1>{data.createdAt}</h1>
-                      </td>
+                    <div className="container-feedbacks">
+                        <div className="titleFDB">
+                            <h1>Avaliações do pedido Nº{id}</h1>
+                        </div>
+                    </div>
 
-                      <td>
-                        {data.avaliacaoId === 1 ? <h1>Atendeu!</h1> : <h1>Não atendeu!</h1>}
-                      </td>
+                    <div className="container-feedbacks">
+                        <>
+                            {feedbacks.status ? (
+                                <>
+                                    <Table className="table-feedbacks" striped bordered hover size="sm">
+                                        <thead>
+                                            <th> Realizado por (nif): </th>
+                                            <th>
+                                                Realizado em:
+                                            </th>
+                                            <th>Status</th>
+                                            <th>
+                                                Detalhes
+                                            </th>
+                                        </thead>
+                                        {feedbacks.list.map((data) => (
+                                            <>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>
+                                                            <h1>{data.userId}</h1>
+                                                        </td>
 
-                      <td>
-                        <Button className="btn-details" onClick={() => { Details(data.feedbackId) }}>detalhes</Button>
-                      </td>
-                    </tr>
-                  </tbody>
+                                                        <td>
+                                                            <h1>{data.createdAt}</h1>
+                                                        </td>
+
+                                                        <td>
+                                                            {data.avaliacaoId === 1 ? <h1>Atendeu!</h1> : <h1>Não atendeu!</h1>}
+                                                        </td>
+
+                                                        <td>
+                                                            <Button className="btn-details" onClick={() => { Details(data.feedbackId) }}>detalhes</Button>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </>
+                                        ))}
+                                    </Table>
+                                </>
+                            ) : (
+                                <>
+                                    <h1>{feedbacks.message}</h1>
+                                </>
+                            )}
+                        </>
+                    </div>
                 </>
-              ))}
-            </Table>
-          </>
-        ) : (
-          <>
-            <h1>{feedbacks.message}</h1>
-          </>
-        )}
-      </>
-    </div>
-  </>
-}
-</>
-  );
+            }
+        </>
+    );
 };
 
 export default RequestList;
