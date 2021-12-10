a2a58e9574de61a1ab3a6d6e08b6e246a341d3ea
Lucas Galdino <lucaas.galdinno@outlook.com>
2021-12-10 10:05:47 -0300

    Atualizando a master. A master estava com o bug do loading infinito

--- src/pages/requestForm/index.js
+++ src/pages/requestForm/index.js
@@ -19,8 +19,6 @@ function RequestForm(props) {
   //cursos
   const [course, setCourse] = useState(0);
 
-  // const [posGraduacao, setPosGraduacao] = useState("");
-  
   const [message, setMessage] = useState("");
 
   const port = process.env.REACT_APP_PORT || 3002;
